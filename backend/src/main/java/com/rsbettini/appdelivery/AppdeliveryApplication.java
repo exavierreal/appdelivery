2a80c974111efdbdc2519ac1631748cce64d1bd7
rsbettini <robson.bettini@hotmail.com>
2021-09-22 16:22:11 -0300

    Project created

---
+++ backend/src/main/java/com/rsbettini/appdelivery/AppdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.rsbettini.appdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rsbettini/appdelivery/AppdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsbettini.appdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
