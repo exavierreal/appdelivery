b19b43eb9c2950a449a4b7334309821672d8860b
rsbettini <robson.bettini@hotmail.com>
2021-10-24 21:33:10 -0300

    Add map delivery

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,15 +1,53 @@
-import React from 'react';
-import { StyleSheet, ScrollView, View} from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ScrollView, Alert, Text, TouchableWithoutFeedback } from 'react-native';
+import { fetchOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { Order } from '../types';
 
 function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetchOrders()
+      .then(response => setOrders(response.data))
+      .catch(error => Alert.alert('Houve um erro ao buscar os pedidos'))
+      .finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      fetchData();
+    }
+  }, [isFocused]);
+
+  const handleOnPress = (order: Order) => {
+    navigation.navigate('OrderDetails', {
+      order
+    });
+  }
+
   return (
     <>
-    <Header />
-    <ScrollView style={styles.container}>
-      <OrderCard />
-    </ ScrollView>
+      <Header />
+      <ScrollView style={styles.container}>
+        {isLoading ? (
+          <Text>Buscando pedidos ...</Text>
+        ) : (
+          orders.map(order => (
+            <TouchableWithoutFeedback
+              key={order.id}
+              onPress={() => handleOnPress(order)}>
+              <OrderCard order={order} />
+            </TouchableWithoutFeedback>
+          ))
+        )}
+      </ ScrollView>
     </>
   );
 }
@@ -19,6 +57,6 @@ const styles = StyleSheet.create({
     paddingRight: '5%',
     paddingLeft: '5%'
   }
-});  
-   
+});
+
 export default Orders;
