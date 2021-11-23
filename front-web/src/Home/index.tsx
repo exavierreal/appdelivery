0e8c08c4419116dc8a2fb1d03e622c053d2edbb3
rsbettini <robson.bettini@hotmail.com>
2021-11-22 22:59:06 -0300

    Fix view Home

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -7,28 +7,28 @@ import { Link } from 'react-router-dom'
 
 function Home() {
     return (
-        <>
-            <div className="home-container">
-            <div className="home-image">
-                        <MainImage />
-                    </div>
-                <div className="home-content">
-                    <div className="home-actions">
-                        <h1 className="home-title">
-                            É só pedir que entregamos <br /> para você!!!
-                        </h1>
-                        <h3 className="home-subtitle">
-                            Faça seu pedido e rapidamente <br /> entregamos em suas mãos.
-                        </h3>
-                        <Link to ="/orders" className="home-btn-order">
-                            FAZER PEDIDO
-                        </Link>
-                    </div>
-                                   </div>
+      <>
+        <div className='home-container'>
+          <div className="home-content">
+            <div className="home-actions">
+              <h1 className='home-title'>
+                É só pedir e saborear!!!
+            </h1>
+              <h3 className='home-subtitle'>
+                Escolha o seu pedido e em poucos <br /> minutos levaremos na sua porta
+            </h3>
+            <Link to='/orders' className='home-btn-order'>
+              FAZER PEDIDO
+            </Link>
             </div>
-            <Footer />
-        </>
-    )
-}
-
-export default Home;
+            <div className="home-image">
+              <MainImage />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </>
+    );
+  }
+  
+  export default Home;
